c387744f1c7da9cdf26c9aa68fac6ae629d6e360
James Duerden <jamie.f.duerden@gmail.com>
2016-03-09 17:11:18 +0000

    bare homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
   # mount ActionCable.server => '/cable' # We're not using this just yet
-  root to: "home#index"
+  root to: "application#home"
 
 end
