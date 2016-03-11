c7754a6564a8ba84a3422b1ba828319bf36e8a92
James Duerden <jamie.f.duerden@gmail.com>
2016-03-11 13:38:33 +0000

    First-pass CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,10 @@
 kiosks = Kiosk.create([
-  {name: 'gp-godalming',  address: 'The Mill Surgery, Catteshall Mill, Catteshall Rd, Godalming, GU7 1JW',   contact: 'Robin Forward - robin.forward@nhs.net'},
-  {name: 'gp-farnham',    address: 'Farnham Centre for Health, Hale Road, Farnham, GU9 9QS',                 contact: 'Deji Bajomo - deji.bajomo@property.nhs.uk'},
-  {name: 'gp-cranleigh',  address: 'Cranleigh Medical Practice, 18 High St, Cranleigh, GU6 8AE',             contact: 'Jackie Stockill - jacqueline.stockill@nhs.net'},
-  {name: 'furniturelink', address: 'Unit 4, Deaconsfield, Cathedral Hill, Guildford, GU2 8YT',               contact: 'Various - 01483 506504'},
-  {name: 'ageuk',         address: 'Age UK, William Road, Guildford, GU1 4QZ',                               contact: 'David Hahn - 01483 503414'},
-  {name: 'waverley',      address: 'Godalming CA, New Montrose House, 36 Bridge Street, Godalming, GU7 1HP', contact: 'Michele Taylor - admin@farnhamcab.cabnet.org.uk'},
-  {name: 'ash',           address: 'Ash CA, Ash Hill Rd, Ash, Aldershot, GU12 5DP',                          contact: ''},
-  {name: 'guildford',     address: 'Guildford CA, 15 Haydon Place, City Centre, Guildford, GU1 4LL',         contact: ''}
+  {name: 'gp-godalming',  address: 'Catteshall Mill, Catteshall Rd, Godalming, GU7 1JW',             contact: 'Robin Forward - robin.forward@nhs.net'},
+  {name: 'gp-farnham',    address: 'Farnham Centre for Health, Hale Road, Farnham, GU9 9QS',         contact: 'Deji Bajomo - deji.bajomo@property.nhs.uk'},
+  {name: 'gp-cranleigh',  address: 'Cranleigh Medical Practice, 18 High St, Cranleigh, GU6 8AE',     contact: 'Jackie Stockill - jacqueline.stockill@nhs.net'},
+  {name: 'furniturelink', address: 'Unit 4, Deaconsfield, Cathedral Hill, Guildford, GU2 8YT',       contact: 'Various - 01483 506504'},
+  {name: 'ageuk',         address: 'Age UK, William Road, Guildford, GU1 4QZ',                       contact: 'David Hahn - 01483 503414'},
+  {name: 'waverley',      address: 'New Montrose House, 36 Bridge Street, Godalming, GU7 1HP',       contact: 'Michele Taylor - admin@farnhamcab.cabnet.org.uk'},
+  {name: 'ash',           address: 'Ash CA, Ash Hill Rd, Ash, Aldershot, GU12 5DP',                  contact: 'Karen Creeth - '},
+  {name: 'guildford',     address: 'Guildford CA, 15 Haydon Place, City Centre, Guildford, GU1 4LL', contact: 'Erica Sandford - '}
   ])
-  
