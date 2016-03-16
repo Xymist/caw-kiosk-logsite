1430f74b3d1a3398bcc77577711e5f4579e056a5
James Duerden <jamie.f.duerden@gmail.com>
2016-03-16 17:34:13 +0000

    homepage progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  def home
 
+  def users
+    user_count = {}
+    last_time = Time.zone.parse('2016-01-01 00:00:01')
+
+    Kiosk.all.each do |kiosk|
+      user_count["#{kiosk.name}"] = 0
+      visits = Visit.where(kiosk: kiosk).order("time_stamp")
+      visits.each do |visit|
+        if visit.time_stamp > last_time + 5.minutes
+          user_count["#{kiosk.name}"] += 1
+          last_time = visit.time_stamp
+        end
+      end
+    end
+    return user_count
+  end
+
+  def home
+    @kiosks = Kiosk.all
+    @users = users
   end
 
   def logs
@@ -29,6 +48,7 @@ class ApplicationController < ActionController::Base
     @title = "Kiosk Data"
     @visits = Visit.all
     @topics = Topic.all
+    @users = users
   end
 
 end
