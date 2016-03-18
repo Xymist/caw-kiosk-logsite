4bda007566e9f01a5065d4bbf95a2f905b31fb6b
James Duerden <jamie.f.duerden@gmail.com>
2016-03-18 12:23:11 +0000

    Mailer job

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160310153928) do
+ActiveRecord::Schema.define(version: 20160318121326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160310153928) do
     t.string   "contact"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "notified"
   end
 
   create_table "topics", force: :cascade do |t|
