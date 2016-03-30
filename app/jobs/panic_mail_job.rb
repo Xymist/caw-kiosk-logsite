7d623253bf6bc36741065f086f50a92181b35ecd
James Duerden <jamie.f.duerden@gmail.com>
2016-03-30 21:45:18 +0100

    Stop complaints

--- app/jobs/panic_mail_job.rb
+++ app/jobs/panic_mail_job.rb
@@ -4,8 +4,8 @@ class PanicMailJob < ApplicationJob
   def perform()
     Kiosk.all.each do |kiosk|
       if kiosk.heartbeats.last                                      # If the kiosk has ever made contact....
-        if kiosk.heartbeats.last.created_at > 15.minutes.ago        # But the last contact was over 15 minutes ago...
-          unless kiosk.notified == true                             # If nobody has been told about it
+        if kiosk.heartbeats.last.created_at < 15.minutes.ago        # But the last contact was over 15 minutes ago...
+          if kiosk.notified == false || kiosk.notified == nil       # If nobody has been told about it
             KioskMailer.panic_email("#{kiosk.name}").deliver_now()  # Send an email
             kiosk.notified = true                                   # Set the flag so we don't send hundreds of emails.
           end
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
-#every 5.minutes do
-#  runner "PanicMailJob.perform_now()"
-#end
+every 5.minutes do
+  runner "PanicMailJob.perform_now()"
+end
 
 every 24.hours do
   runner "DatabaseStufferJob.perform_now()"
