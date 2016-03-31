f4643e11e14449d5681a8836adca23800f00716e
James Duerden <jamie.f.duerden@gmail.com>
2016-03-31 13:38:21 +0100

    Force status update

--- app/jobs/panic_mail_job.rb
+++ app/jobs/panic_mail_job.rb
@@ -7,10 +7,12 @@ class PanicMailJob < ApplicationJob
         if kiosk.heartbeats.last.created_at < 15.minutes.ago        # But the last contact was over 15 minutes ago...
           if kiosk.notified == false || kiosk.notified == nil       # If nobody has been told about it
             KioskMailer.panic_email("#{kiosk.name}").deliver_now()  # Send an email
-            kiosk.notified = true                                   # Set the flag so we don't send hundreds of emails.
+            kiosk.notified = true
+            kiosk.save                                              # Set the flag so we don't send hundreds of emails.
           end
         else                                                        # If the kiosk has been in contact more recently than 15 minutes ago...
-          kiosk.notified = false                                    # Reset; either there has never been a problem or the problem has been solved.
+          kiosk.notified = false
+          kiosk.save                                                # Reset; either there has never been a problem or the problem has been solved.
         end
       end
     end
