65942ac5020356d6edf21bd3a41d47cdfc96c235
James Duerden <jamie.f.duerden@gmail.com>
2016-04-13 18:22:29 +0100

    Ignore blank lines in logfiles

--- app/jobs/database_stuffer_job.rb
+++ app/jobs/database_stuffer_job.rb
@@ -5,7 +5,9 @@ class DatabaseStufferJob < ApplicationJob
   def perform()
     require 'date'
     Dir.glob('app/assets/kiosk_logs/*.log').each do |logfile|
+      begin
       logname = logfile.split("/")
+      rescue NoMethodError
       kiosk = logname[-1].split(".")
       access_data = IO.readlines(logfile)
       access_data.each do |data|
@@ -16,7 +18,6 @@ class DatabaseStufferJob < ApplicationJob
         host = Host.find_or_create_by(name: hostname)
         topic = host.topics.find_or_create_by(location: topicnames[0].chomp)
         kiosk_name = Kiosk.find_or_create_by(name: kiosk[0])
-        begin
         topic.visits.find_or_create_by(time_stamp: time_stamp, kiosk_id: kiosk_name.id, checksum: Digest::MD5.hexdigest("#{time_stamp}|#{kiosk_name}"))
         rescue ActiveRecord::RecordNotUnique
         end
