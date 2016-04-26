95ae6995edeba4733b30a25c0e87b9f6a7eb325f
James Duerden <jamie.f.duerden@gmail.com>
2016-04-26 21:55:31 +0100

    More robust internal logging

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,8 +2,8 @@ every 5.minutes do
   runner "PanicMailJob.perform_now()"
 end
 
-every 2.hours do
+every 20.minutes do
   runner "DatabaseStufferJob.perform_now()"
 end
 
-set :output, "log/cron_log.log"
+set :output, "/home/caw/apps/caw-kiosk-logsite/log/cron_log.log"
