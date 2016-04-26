aa7cb6bff7a00d547ba8e239e66e02d5de096f99
James Duerden <jamie.f.duerden@gmail.com>
2016-04-26 20:09:40 +0100

    Fixed visit recording

--- app/jobs/database_stuffer_job.rb
+++ app/jobs/database_stuffer_job.rb
@@ -18,7 +18,11 @@ class DatabaseStufferJob < ApplicationJob
         hostname = split_data[1]
         topicnames = split_data[2].split(',')
         host = Host.find_or_create_by(name: hostname)
-        topic = host.topics.find_or_create_by(location: topicnames[0].chomp)
+        if host.name == "82.70.248.237"
+          topic = host.topics.find_or_create_by(location: topicnames[2].chomp)
+        else
+          topic = host.topics.find_or_create_by(location: topicnames[0].chomp)
+        end
         kiosk_name = Kiosk.find_or_create_by(name: @kiosk[0])
         begin
         topic.visits.find_or_create_by(time_stamp: time_stamp, kiosk_id: kiosk_name.id, checksum: Digest::MD5.hexdigest("#{time_stamp}|#{kiosk_name}"))
