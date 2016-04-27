295a9a31d48ef58ba6049e6499cf3dcf1def9dfa
James Duerden <jamie.f.duerden@gmail.com>
2016-04-27 12:53:17 +0100

    Internal logging functions (nearly)

--- app/controllers/public_kiosk_controller.rb
+++ app/controllers/public_kiosk_controller.rb
@@ -18,13 +18,14 @@ class PublicKioskController < ActionController::Base
   def exit_site
     new_url = AdvicePage.find_by(id: params[:exit_url_id]).url
     split_url = new_url.sub(/^https?\:\/\/(www.)?/,'').split('/')
-    new_host = new_url[0]
-    new_topic = new_url[1]
+    new_host = split_url[0]
+    new_topic = split_url[1]
+    time_stamp = Time.now
     host = Host.find_or_create_by(name: new_host)
-    topic = host.topics.find_or_create_by(location: new_topic.chomp)
+    topic = new_topic ? host.topics.find_or_create_by(location: new_topic.chomp) : Topic.find_or_create_by(location: params[:topic], host: host)
     kiosk = Kiosk.find_or_create_by(name: params[:kiosk])
     begin
-      topic.visits.find_or_create_by(time_stamp: time_stamp, kiosk_id: kiosk.id, checksum: Digest::MD5.hexdigest("#{time_stamp}|#{kiosk_name}"))
+      topic.visits.find_or_create_by(time_stamp: time_stamp, kiosk_id: kiosk.id, checksum: Digest::MD5.hexdigest("#{time_stamp}|#{kiosk.name}"))
     rescue ActiveRecord::RecordNotUnique
       # find_or_create_by should obviate this, but it's still here because things break otherwise.
     end
