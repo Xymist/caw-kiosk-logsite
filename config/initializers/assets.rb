6169229aa39bf76b00144ae8059cf7e3ba6ebb66
James Duerden <jamie.f.duerden@gmail.com>
2016-07-01 17:04:38 +0100

    Fix some Turbolinks bugs

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,15 +9,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( noVNC/util.js )
-Rails.application.config.assets.precompile += %w( noVNC/webutil.js )
-Rails.application.config.assets.precompile += %w( noVNC/base64.js )
-Rails.application.config.assets.precompile += %w( noVNC/websock.js )
-Rails.application.config.assets.precompile += %w( noVNC/des.js )
-Rails.application.config.assets.precompile += %w( noVNC/input.js )
-Rails.application.config.assets.precompile += %w( noVNC/display.js )
-Rails.application.config.assets.precompile += %w( noVNC/rfb.js )
-Rails.application.config.assets.precompile += %w( noVNC/jsunzip.js )
-Rails.application.config.assets.precompile += %w( noVNC/playback.js )
-Rails.application.config.assets.precompile += %w( noVNC/ui.js )
-Rails.application.config.assets.precompile += %w( noVNC/logo.js )
